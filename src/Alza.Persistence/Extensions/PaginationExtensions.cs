c90a8dba2f398de727bc3e0573e003dbffcf63e3
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-12-03 16:05:29 +0100

    Implement offset pagination

---
+++ src/Alza.Persistence/Extensions/PaginationExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Alza.Persistence.Extensions;
+
+internal static class PaginationExtensions
+{
+    public static IQueryable<T> Paginate<T>(this IQueryable<T> ts, int pageNumber, int pageSize)
+    {
+        return ts.Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize);
+    }
+}
