a6da0c0e43d9bdba270587ba7103ef4d4339c651
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-12-03 14:52:15 +0100

    Create controller skeleton and dtos

---
+++ src/Alza.Api/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Alza.Api.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alza.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<ProductResponse>> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id:guid}")]
+    public ActionResult<ProductResponse> GetProductById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpdateProductDescription(Guid id, [FromBody] UpdateProductDescriptionRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Alza.Api/Dtos/ProductDtos.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alza.Api.Dtos;
+
+public class ProductResponse
+{
+    [Required]
+    public Guid Id { get; init; }
+
+    [Required] 
+    public string Name { get; init; } = null!;
+    
+    [Required]
+    public decimal Price { get; init; }
+    
+    public string? Description { get; init; }
+}
+
+public class UpdateProductDescriptionRequest
+{
+    [Required]
+    public string Description { get; init; } = null!;
+}
